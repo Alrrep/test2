370923afb2eaad91889eb56302dd5c8673f39c1f
Alrrep <juan.daniel.rangel.avila@gmail.com>
2023-09-25 13:24:08 -0600

    try except implements

--- example1.py
+++ example1.py
@@ -3,6 +3,15 @@
 # GNU/GPL License
 
 import numpy as np
-x=np.pi
-z=np.cos(x)
-print(z)
+data_list=np.arange(1,11,1)
+for x in data_list:
+    print("value: " + str(x))
+#z=np.cos(x)
+x="Hola"
+
+try:
+    z=int(x)+1
+except:
+    print("Error in sum of z.")
+
+print(data_list)
